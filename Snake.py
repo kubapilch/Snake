70b3108a03ae04e0435a5d5e4c6c9ffebf15451f
kubapilch <kuba.pilch2001@gmail.com>
2017-10-01 17:10:22 +0200

    Food bug fix

--- Snake.py
+++ Snake.py
@@ -13,274 +13,282 @@ food = [1,1]
 score = 0
 
 def set_up_variables():
-	
-	#Set joystick functions
-	sense.stick.direction_up = go_up
-	sense.stick.direction_down = go_down
-	sense.stick.direction_left = go_left
-	sense.stick.direction_right = go_right
+    
+    #Set joystick functions
+    sense.stick.direction_up = go_up
+    sense.stick.direction_down = go_down
+    sense.stick.direction_left = go_left
+    sense.stick.direction_right = go_right
 
 
-	atexit.register(exint_handler)
+    atexit.register(exint_handler)
 
 
 def exint_handler():
-	#Clear leds
-	sense.clear()
+    #Clear leds
+    sense.clear()
 
 def game_over():
-	global score 
-	print("Game over, your score = %s" % score)
+    global score 
+    print("Game over, your score = %s" % score)
 
 def move():
-	global snake,food,score
+    global snake,food,score
 
-	while True:
+    while True:
 
-		head_x = snake.positions[0][0]
-		head_y = snake.positions[0][1]
+        head_x = snake.positions[0][0]
+        head_y = snake.positions[0][1]
 
-		checker = 0
-		last_position = [0,0]
+        checker = 0
+        last_position = [0,0]
 
-		#If snake direction is Up
-		if snake.direction == 1:
-			
-			#Check if next postion isnt wall or snake tail
-			if head_y < 1 or [head_x,head_y - 1] in snake.positions:
-				game_over()
-				return
+        #If snake direction is Up
+        if snake.direction == 1:
+            
+            #Check if next postion isnt wall or snake tail
+            if head_y < 1 or [head_x,head_y - 1] in snake.positions:
+                game_over()
+                return
 
-			#Move snake in array
-			for i in snake.postions:
-				#If its head
-				if checker == 0:
-					last_position = i
+            #Move snake in array
+            for i in snake.postions:
+                #If its head
+                if checker == 0:
+                    last_position = i
 
-					snake.positions[checker] = [head_x,head_y - 1]
+                    snake.positions[checker] = [head_x,head_y - 1]
 
-					checker += 1
+                    checker += 1
 
-					continue
+                    continue
 
-				#It is not head
-				new_position = last_position
+                #It is not head
+                new_position = last_position
 
-				last_position = i
+                last_position = i
 
-				snake.positions[checker] = new_position
+                snake.positions[checker] = new_position
 
-				checker += 1
+                checker += 1
 
-			#Check if snake ate food
-			if food == snake.postions[0]:
-				#Snake has eaten food so add new elemnt to snake 
-				snake.positions += last_position
+            #Check if snake ate food
+            if food == snake.postions[0]:
+                #Snake has eaten food so add new elemnt to snake 
+                snake.positions += last_position
 
-				#Random new food
-				random_food
+                score += 1
 
-		#If snake direction is Right
-		if snake.direction == 2:
-			
-			#Check if next postion isnt wall or snake tail
-			if head_x > 6 or [head_x + 1,head_y] in snake.positions:
-				game_over()
-				return
+                #Random new food
+                random_food
 
-			#Move snake in array
-			for i in snake.postions:
-				#If its head
-				if checker == 0:
-					last_position = i
+        #If snake direction is Right
+        if snake.direction == 2:
+            
+            #Check if next postion         
+            if head_x > 6 or [head_x + 1,head_y] in snake.positions:
+                game_over()
+                return
 
-					snake.positions[checker] = [head_x + 1,head_y]
+            #Move snake in array
+            for i in snake.postions:
+                #If its head
+                if checker == 0:
+                    last_position = i
 
-					checker += 1
+                    snake.positions[checker] = [head_x + 1,head_y]
 
-					continue
+                    checker += 1
 
-				#It is not head
-				new_position = last_position
+                    continue
 
-				last_position = i
+                #It is not head
+                new_position = last_position
 
-				snake.positions[checker] = new_position
+                last_position = i
 
-				checker += 1
+                snake.positions[checker] = new_position
 
-			#Check if snake ate food
-			if food == snake.postions[0]:
-				#Snake has eaten food so add new elemnt to snake 
-				snake.positions += last_position
+                checker += 1
 
-				#Random new food
-				random_food
+            #Check if snake ate food
+            if food == snake.postions[0]:
+                #Snake has eaten food so add new elemnt to snake 
+                snake.positions += last_position
 
-		#If snake direction is Down
-		if snake.direction == 3:
-			
-			#Check if next postion isnt wall or snake tail
-			if head_y > 6 or [head_x,head_y + 1] in snake.positions:
-				game_over()
-				return
+                score += 1
 
-			#Move snake in array
-			for i in snake.postions:
-				#If its head
-				if checker == 0:
-					last_position = i
+                #Random new food
+                random_food
 
-					snake.positions[checker] = [head_x,head_y + 1]
+        #If snake direction is Down
+        if snake.direction == 3:
+            
+            #Check if next postion isnt wall or snake tail
+            if head_y > 6 or [head_x,head_y + 1] in snake.positions:
+                game_over()
+                return
 
-					checker += 1
+            #Move snake in array
+            for i in snake.postions:
+                #If its head
+                if checker == 0:
+                    last_position = i
 
-					continue
+                    snake.positions[checker] = [head_x,head_y + 1]
 
-				#It is not head
-				new_position = last_position
+                    checker += 1
 
-				last_position = i
+                    continue
 
-				snake.positions[checker] = new_position
+                #It is not head
+                new_position = last_position
 
-				checker += 1
+                last_position = i
 
-			#Check if snake ate food
-			if food == snake.postions[0]:
-				#Snake has eaten food so add new elemnt to snake 
-				snake.positions += last_position
+                snake.positions[checker] = new_position
 
-				#Random new food
-				random_food
+                checker += 1
 
-		#If snake direction is Left
-		if snake.direction == 4:
-			
-			#Check if next postion isnt wall or snake tail
-			if head_x < 1 or [head_x - 1,head_y] in snake.positions:
-				game_over()
-				return
+            #Check if snake ate food
+            if food == snake.postions[0]:
+                #Snake has eaten food so add new elemnt to snake 
+                snake.positions += last_position
 
-			#Move snake in array
-			for i in snake.postions:
-				#If its head
-				if checker == 0:
-					last_position = i
+                score += 1
 
-					snake.positions[checker] = [head_x - 1,head_y]
+                #Random new food
+                random_food
 
-					checker += 1
+        #If snake direction is Left
+        if snake.direction == 4:
+            
+            #Check if next postion isnt wall or snake tail
+            if head_x < 1 or [head_x - 1,head_y] in snake.positions:
+                game_over()
+                return
 
-					continue
+            #Move snake in array
+            for i in snake.postions:
+                #If its head
+                if checker == 0:
+                    last_position = i
 
-				#It is not head
-				new_position = last_position
+                    snake.positions[checker] = [head_x - 1,head_y]
 
-				last_position = i
+                    checker += 1
 
-				snake.positions[checker] = new_position
+                    continue
 
-				checker += 1
+                #It is not head
+                new_position = last_position
 
-			#Check if snake ate food
-			if food == snake.postions[0]:
-				#Snake has eaten food so add new elemnt to snake 
-				snake.positions += last_position
+                last_position = i
 
-				#Random new food
-				random_food
+                snake.positions[checker] = new_position
 
-		#Set new layout
-		layout()
+                checker += 1
 
-		#Wait 
-		sleep(0.5)
+            #Check if snake ate food
+            if food == snake.postions[0]:
+                #Snake has eaten food so add new elemnt to snake 
+                snake.positions += last_position
+
+                score += 1
+
+                #Random new food
+                random_food
+
+        #Set new layout
+        layout()
+
+        #Wait 
+        sleep(0.5)
 
 def random_food():
-	global snake,food
+    global snake,food
 
-	#Random food position 
-	while food in snake.positions:
-		x = randint(0,7)
-		y = randint(0,7)
+    #Random food position 
+    while food in snake.positions:
+        x = randint(0,7)
+        y = randint(0,7)
 
-		food = [x,y]
+        food = [x,y]
 
 def start_game():
 
-	#random first food
-	random_food()
+    #random first food
+    random_food()
 
-	#Set up first layout
-	layout()
+    #Set up first layout
+    layout()
 
-	#Start the game
-	move()
+    #Start the game
+    move()
 
 def layout():
-	global snake,food
-	
-	#Clear old layout
-	sense.clear()
+    global snake,food
+    
+    #Clear old layout
+    sense.clear()
 
-	#incrementation checker
-	checker = 0
+    #incrementation checker
+    checker = 0
 
-	#Layout new snake postions 
-	for i in snake.positions:
-		#X pos
-		x = i[0]
+    #Layout new snake postions 
+    for i in snake.positions:
+        #X pos
+        x = i[0]
 
-		#Y pos
-		y = i[1]
+        #Y pos
+        y = i[1]
 
-		#Led color
-		color = snake.color
+        #Led color
+        color = snake.color
 
-		if checker == 0:
-			color = snake.head_color
+        if checker == 0:
+            color = snake.head_color
 
-		sense.set_pixel(x,y,color)
+        sense.set_pixel(x,y,color)
 
-	#Layout food
-	sense.set_pixel(food[0],food[1],255,0,0)
+    #Layout food
+    sense.set_pixel(food[0],food[1],255,0,0)
 
 def go_up(event):
-	global snake
-	
-	if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
-		return
+    global snake
+    
+    if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
+        return
 
-	#Chenge direction to Up
-	 snake.direction = 1
+    #Chenge direction to Up
+     snake.direction = 1
 
 def go_right(event):
-	global snake
-	
-	if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
-		return
+    global snake
+    
+    if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
+        return
 
-	#Change direction to Right
-	snake.direction = 2
+    #Change direction to Right
+    snake.direction = 2
 
 def go_down(event):
-	global snake
-	
-	if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
-		return
+    global snake
+    
+    if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
+        return
 
-	#Change direction to Down
-	snake.direction = 3
+    #Change direction to Down
+    snake.direction = 3
 
 def go_left(event):
-	global snake
-	
-	if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
-		return
+    global snake
+    
+    if event.action == ACTION_RELEASED or event.action == ACTION_HELD:
+        return
 
-	#Change direction to Left
-	snake.direction = 4
+    #Change direction to Left
+    snake.direction = 4
 
 start_game()
 
