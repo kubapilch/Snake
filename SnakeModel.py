e2d2d26f3942d63111b9165ae4e06b3ffeb6f0ee
kubapilch <kuba.pilch2001@gmail.com>
2017-10-01 17:25:25 +0200

    Bug fix

---
+++ SnakeModel.py
@@ -0,0 +1,8 @@
+
+class Snake():
+
+	#snake variables
+	positions = [[4,4],[4,5],[4,6]] #Starts with 3 blocks
+	direction = 1 # 1=Up,2=right,3=down and 4 = left
+	color = (0,255,0) 
+	head_color = (0,0,255)
